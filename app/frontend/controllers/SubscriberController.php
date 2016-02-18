12290f1a72ac9796df7851162bf621f57484a1f2
Suneesh Arumugham Pillai <spillai@corp.broadsoft.com>
2016-02-18 16:56:54 +0530

    first commit

---
+++ app/frontend/controllers/SubscriberController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class SubscriberController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
