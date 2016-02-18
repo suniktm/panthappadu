75b450afef146b609993d5dae2842862153ac10b
Suneesh Arumugham Pillai <spillai@corp.broadsoft.com>
2016-02-18 17:32:06 +0530

    First commit

---
+++ app/backend/models/SubscriberSearch.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Subscriber;
+
+/**
+ * SubscriberSearch represents the model behind the search form about `app\models\Subscriber`.
+ */
+class SubscriberSearch extends Subscriber
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'status'], 'integer'],
+            [['username', 'email', 'pwd'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Subscriber::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'status' => $this->status,
+        ]);
+
+        $query->andFilterWhere(['like', 'username', $this->username])
+            ->andFilterWhere(['like', 'email', $this->email])
+            ->andFilterWhere(['like', 'pwd', $this->pwd]);
+
+        return $dataProvider;
+    }
+}
